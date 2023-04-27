3cff457dd5fd7c26b66a4c04df822c9b9e30f3e9
Leo <eta.development.leo@outlook.com>
2023-04-27 17:54:30 +0300

    tasks_oge

---
+++ oge_pascal/Variant1_15.2.pas
@@ -0,0 +1,19 @@
+﻿program Variant1_15;
+
+var
+  k: integer;
+  n: integer;
+  sum := 0;
+
+begin
+  Readln(k);
+  for var x := 0 to k - 1 do
+  begin
+    Readln(n);
+    if (n < 30) and (n mod 2 = 0) then
+    begin
+      sum := sum + n
+    end;
+  end;
+  Writeln('sum = ', sum);
+end.
